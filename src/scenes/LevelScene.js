bc045abfd697b63a2c5eb5694c468fbb6002a290
trusovskii <dmitry.dulinets@yandex.ru>
2022-06-15 16:26:12 +0400

    Fix text

--- src/scenes/LevelScene.js
+++ src/scenes/LevelScene.js
@@ -377,7 +377,7 @@ export default class LevelScene extends Phaser.Scene {
   }
 
   createPlayer() {
-    const player = this.physics.add.sprite(2200, 550 + VERTICAL_OFFSET);
+    const player = this.physics.add.sprite(14400, 550 + VERTICAL_OFFSET);
     player.body.setSize(40, 200);
     player.setScale(0.4, 0.4);
     player.body.setOffset(120, 40);
